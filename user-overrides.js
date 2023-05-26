a1ad19997980758efe25460815c4d14b2e9ff1cf
GhoulBoii <78494833+GhoulBoii@users.noreply.github.com>
2023-05-26 20:19:02 +0530

    adding files

---
+++ user-overrides.js
@@ -0,0 +1,14 @@
+user_pref("keyword.enabled", true);
+user_pref("browser.startup.page", 3);
+// user_pref("security.OCSP.require", true);
+user_pref("privacy.sanitize.sanitizeOnShutdown", false);
+user_pref("privacy.resistFingerprinting", false);
+user_pref("privacy.resistFingerprinting.letterboxing", false); // [HIDDEN PREF]
+user_pref("browser.display.use_system_colors", false); // [DEFAULT: false NON-WINDOWS]
+user_pref("webgl.disabled", false);
+user_pref("extensions.pocket.enabled", false);
+user_pref("browser.tabs.firefox-view", false);
+user_pref("general.autoScroll", true);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("signon.rememberSignons", false);
+user_pref("network.http.referer.XOriginPolicy", 0);
