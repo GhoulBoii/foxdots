8c3f47eb9b52f2d9490ce06779b7ca91913eac16
Kshamendra <github@ghoulcloud.slmail.me>
2024-04-01 00:51:43 +0530

    style(user-overrides): adding comments and new rule

--- user-overrides.js
+++ user-overrides.js
@@ -1,14 +1,15 @@
-user_pref("browser.startup.page", 3);
-//user_pref("security.OCSP.require", false);
-user_pref("privacy.sanitize.sanitizeOnShutdown", false);
-user_pref("privacy.resistFingerprinting", false);
-user_pref("privacy.resistFingerprinting.letterboxing", false); // [HIDDEN PREF]
-user_pref("webgl.disabled", false);
+// Privacy-Focused
+user_pref("browser.startup.page", 3);                                           // Resumes previous session
+user_pref("privacy.sanitize.sanitizeOnShutdown", false);                        // Disable cleraing items on shutdown
+user_pref("privacy.resistFingerprinting", false);                               // Disable RFP
+user_pref("privacy.resistFingerprinting.letterboxing", false);                  // Disable letterboxing 
+user_pref("webgl.disabled", false);                                             // Enable WebGL
 
-user_pref("browser.display.use_system_colors", false); // [DEFAULT: false NON-WINDOWS]
-user_pref("extensions.pocket.enabled", false);
-user_pref("browser.tabs.firefox-view", false);
-user_pref("general.autoScroll", true);
-user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-user_pref("signon.rememberSignons", false);
-
+// Customisation
+user_pref("browser.display.use_system_colors", false);                          // Do not use default dark mode or light mode
+user_pref("extensions.pocket.enabled", false);                                  // Disable Pocket
+user_pref("browser.tabs.firefox-view", false);                                  // Disable Firefox View
+user_pref("general.autoScroll", true);                                          // Enable AutoScrolling
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);         // Enable userchrome.css
+user_pref("signon.rememberSignons", false);                                     // Disable saving passwords
+user_pref("browser.urlbar.update2.engineAliasRefresh", true);                   // Add button to add custom search engines
