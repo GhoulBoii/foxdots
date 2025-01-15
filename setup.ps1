82d3a44dd0e4a2abf5b86d0aed715d57057823f0
GhoulBoii <code.ksh@erine.email>
2025-01-15 11:06:14 +0530

    refactor: rewrote shell & powershell scripts for readability

--- setup.ps1
+++ setup.ps1
@@ -1,30 +1,26 @@
-# Prompt for Firefox profile name
-Write-Host "Enter name of the Firefox profile: " -ForegroundColor Green -NoNewline
-$name = Read-Host
-
-# Read JSON configuration file
+# Read config file
 $jsonContent = Get-Content -Raw -Path "config.json" | ConvertFrom-Json
-$addonList = $jsonContent.addonlist -split ','
-
-# Constants for paths
-$firefoxProfilePath = "$env:APPDATA\Mozilla\Firefox"
-$profilesIniPath = "$firefoxProfilePath\profiles.ini"
-$mozillaAddonURL = "https://addons.mozilla.org"
+Set-Variable addonsList -Option Constant -Value $jsonContent.addons_list -
+$addonsList = $jsonContent.addons_list -split ','
 
 function Create-Profile {
     Write-Host "Creating Profile..." -ForegroundColor Green
 
-    # Create the Firefox profile
+    Set-Variable firefoxProfilePath -Option Constant -Value "$env:APPDATA\Mozilla\Firefox"
+    Set-Variable profilesIniPath -Option Constant -Value "$firefoxProfilePath\profiles.ini"
+
+    Write-Host "Enter name of the firefox profile: " -ForegroundColor Green -NoNewline
+    $name = Read-Host
     Start-Process "firefox" -ArgumentList "-CreateProfile $name"
     Start-Sleep -Seconds 1  # Wait for Firefox to update profile.ini
 
-    # Find the newly created profile directory
+    # Find and cd into the new profile
     $folder = (Get-Content $profilesIniPath | Select-String -Pattern "Path=.*$name$" | ForEach-Object { $_ -replace 'Path=', '' })
     $profileDir = Join-Path -Path $firefoxProfilePath "$folder"
-
-    # Set working directory to profile folder
     Set-Location -Path $profileDir
+
     Write-Host "Profile Creation Finished" -ForegroundColor Green
+    return $profileDir
 }
 
 function Init-GitRepo {
@@ -32,36 +28,36 @@ function Init-GitRepo {
 
     # Initialize the Git repository and fetch updates
     git init
-    git remote add origin https://github.com/ghoulboii/firefox
+    git remote add origin https://github.com/ghoulboii/foxdots
     git fetch
-    git checkout origin/master -ft
+    git checkout --force --track origin/master
     git submodule update --init --recursive --remote
 
-    # Copy necessary files
     robocopy userjs . user.js prefsCleaner.bat updater.bat
-    robocopy VerticalFox\windows\ chrome
-    robocopy VerticalFox\sidebery\ sidebery
 
-    # Run updater script
     .\updater.bat -unattended
 
     Write-Host "Git Repository Initialized" -ForegroundColor Green
 }
 
 function Download-Addons {
+    param (
+        [string]$profileDir
+    )
     Write-Host "Downloading Addons..." -ForegroundColor Green
 
-    # Create temporary folder for downloading add-ons
+    Set-Variable mozillaAddonURL -Option Constant -Value "https://addons.mozilla.org"
     $addonTmpDir = New-Item -ItemType Directory -Path "$env:TEMP\addon" -Force
+    $extensionsDir = New-Item -ItemType Directory -Path "$profileDir\extensions" -ErrorAction SilentlyContinue
 
-    foreach ($addon in $addonList) {
+    foreach ($addon in $addonsList) {
         $addon = $addon.Trim()
 
         Write-Host "Installing addon: $addon" -ForegroundColor Green
 
         # Get addon download URL
         $addonPageContent = Invoke-WebRequest -Uri "$mozillaAddonURL/en-US/firefox/addon/$addon/" -UseBasicParsing
-        $addonDownloadURL = $addonPageContent.Content | Select-String -Pattern "$mozillaAddonURL/firefox/downloads/file/[^`"]*" | ForEach-Object { $_.Value }
+        $addonDownloadURL = $addonPageContent.Content | Select-String -Pattern "$mozillaAddonURL/firefox/downloads/file/[^`"]*" | Select-Object -ExpandProperty Matches | ForEach-Object { $_.Value }
 
         # Download and extract the add-on
         $addonFileName = $addonDownloadURL -split '/' | Select-Object -Last 1
@@ -72,19 +68,13 @@ function Download-Addons {
         Expand-Archive -Path $addonDownloadPath -DestinationPath $addonExtractDir -Force
 
         # Extract addon ID and move to profile extensions folder
-        $addonId = (Select-String -Path "$addonExtractDir/manifest.json" -Pattern '"id"\s*:\s*"([^"]*)"' -Raw | ForEach-Object { $_ -replace '\s*"id":\s*"([^"]*)"', '$1' }).Trim()
-
-        # Ensure extensions directory exists and move the add-on
-        $extensionsDir = Join-Path -Path $profileDir -ChildPath "extensions"
-        New-Item -ItemType Directory -Path $extensionsDir -ErrorAction SilentlyContinue
-
+        $addonId = (Select-String -Path "$addonExtractDir/manifest.json" -Pattern '"id"' -Raw | ForEach-Object { $_ -replace '\s*"id":\s*"([^"]*)"', '$1' }).Trim(',')
         Move-Item -Path "$addonTmpDir\$addonFileName" -Destination "$extensionsDir\$addonId.xpi" -Force
     }
 
     Write-Host "Addons Installed" -ForegroundColor Green
 }
 
-# Run the functions
-Create-Profile
+$profileDir = Create-Profile
 Init-GitRepo
-Download-Addons
+Download-Addons -profileDir $profileDir
--- setup.sh
+++ setup.sh
@@ -1,43 +1,89 @@
 #!/bin/sh
 
-echo -n "Enter name of the firefox profile: " && read name
+input_profile_name() {
+  echo -n "Enter name of the Firefox profile: "
+  read name
+  echo "$name"
+}
 
-# Read the JSON file and extract the extension list as a single string
-extension_lists=$(grep -oP '"extension_lists":\s*\[\K[^\]]+' extensions.json | tr -d ' ",')
+create_profile() {
+  declare -r name="$1"
+  echo "Creating Profile"
+  firefox -CreateProfile "$name"
 
-echo "Creating Profile"
-firefox -CreateProfile $name
-folder=$(sed -n "/Path=.*.$name$/ s/.*=//p" ~/.mozilla/firefox/profiles.ini)
-path="/home/$(whoami)/.mozilla/firefox/$folder"
-cd $path
-echo "Profile Creation Finished"
+  # Get the folder path of the newly created profile
+  local folder=$(sed -n "/Path=.*\.$name$/ s/.*=//p" ~/.mozilla/firefox/profiles.ini)
+  local path="/home/$(whoami)/.mozilla/firefox/$folder"
 
-echo "Initialising Git Repo"
-git init
-git remote add origin https://github.com/ghoulboii/firefox
-git fetch
-git checkout origin/master -ft
-git submodule update --init --recursive --remote
-cp userjs/{updater.sh,prefsCleaner.sh,user.js} .
-cp -r VerticalFox/{windows,sidebery} .
-mv windows chrome
-./updater.sh -s
-echo "Git Repo Initialised"
+  # Change to profile directory
+  cd "$path" || exit 1
+  echo "Profile Creation Finished"
+  echo "$path"
+}
 
-echo "Downloading Addons"
-addontmp="$(mktemp -d)"
-trap "rm -fr $addontmp" HUP INT QUIT TERM PWR EXIT
-mozillaurl="https://addons.mozilla.org"
-IFS=$'\n,'
-mkdir -p "$path/extensions/"
-for addon in $addonlist; do
-	echo "Installing $addon"
-	addonurl="$(curl --silent "$mozillaurl/en-US/firefox/addon/${addon}/" | grep -o "$mozillaurl/firefox/downloads/file/[^\"]*")"
-	file="${addonurl##*/}"
-	curl -LOs "$addonurl" >"$addontmp/$file"
-	id="$(unzip -p "$file" manifest.json | grep "\"id\"")"
-	id="${id%\"*}"
-	id="${id##*\"}"
-	mv "$file" "$path/extensions/$id.xpi"
-done
-echo "Addons Installed"
+initialize_git_repo() {
+  echo "Initializing Git Repo"
+  git init
+  git remote add origin https://github.com/ghoulboii/firefox
+  git fetch
+  git checkout origin/master -ft
+  git submodule update --init --recursive --remote
+
+  cp userjs/updater.sh .
+  cp userjs/prefsCleaner.sh .
+  cp userjs/user.js .
+  cp -r VerticalFox/windows .
+  cp -r VerticalFox/sidebery .
+
+  mv windows chrome
+
+  ./updater.sh -s
+  echo "Git Repo Initialized"
+}
+
+install_addons() {
+  declare -r path="$1"
+  declare -r addons_list="$2"
+  declare -r mozillaurl="https://addons.mozilla.org"
+  echo "Downloading Addons"
+
+  # Create a temporary directory for downloading addons
+  addontmp=$(mktemp -d)
+  trap "rm -fr $addontmp" HUP INT QUIT TERM PWR EXIT
+
+  mkdir -p "$path/extensions/"
+
+  # Iterate over the addon list
+  IFS=','  # Use comma as delimiter for addon list
+  for addon in $addons_list; do
+    addon=$(echo "$addon" | tr -d ' ')  # Remove any spaces
+
+    echo "Installing $addon"
+
+    # Get the download URL for the addon
+    addonurl=$(curl --silent "$mozillaurl/en-US/firefox/addon/$addon/" | grep -o "$mozillaurl/firefox/downloads/file/[^\"]*")
+    file="${addonurl##*/}"
+
+    # Download the addon
+    curl -LOs "$addonurl" > "$addontmp/$file"
+
+    # Extract the addon ID from the manifest.json
+    id=$(unzip -p "$addontmp/$file" manifest.json | grep -o '"id": *"[^"]*' | sed 's/"id": *"//')
+
+    # Move the downloaded addon to the profile extensions directory
+    mv "$addontmp/$file" "$path/extensions/$id.xpi"
+  done
+  echo "Addons Installed"
+}
+
+main() {
+  name=$(input_profile_name)
+  # Read the extension list from config
+  extension_list=$(grep -oP '"addons_list":\s*\[\K[^\]]+' config.json | tr -d ' ",')
+
+  profile_path=$(create_profile "$name")
+  initialize_git_repo
+  install_addons "$profile_path" "$addons_list"
+}
+
+main
